81936ac0775336483f4d6c0e1ee76125c11f0992
siddhes07 <siddheshnj07@gmail.com>
2025-11-10 22:32:13 +0530

    Add Reverse.c to print numbers in reverse order reverse

---
+++ for loop/Reverse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Using for loop to print numbers in reverse
+    for (i = 10; i >= 1; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
