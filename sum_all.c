0fa28b9326ecbf1a0cf6404dcae06bcbaf6878be
siddhes07 <siddheshnj07@gmail.com>
2025-11-11 19:54:55 +0530

    Add files via upload

---
+++ sum_all.c
@@ -0,0 +1,49 @@
+//Dynamically allocate and read an array of size n. Find the sum and average of all
+//elements in this array..
+
+#include <stdio.h>
+#include <stdlib.h>  
+
+int main() {
+    int n, i;
+    float sum = 0, avg;
+    int *arr;
+
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    
+    arr = (int *)malloc(n * sizeof(int));
+
+    
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    
+    avg = sum / n;
+
+    
+    printf("Sum = %.2f\n", sum);
+    printf("Average = %.2f\n", avg);
+
+
+    free(arr);
+
+    return 0;
+}
+
+
+
+
+
+
