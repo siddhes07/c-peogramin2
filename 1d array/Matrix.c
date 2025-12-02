01bcf2fd5bcf051cc2d9b2b6ca6638722603c8db
siddhes07 <siddheshnj07@gmail.com>
2025-12-02 23:44:44 +0530

    Implement matrix multiplication in Matrix.c

---
+++ 1d array/Matrix.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], mul[10][10];
+    int r1, c1, r2, c2;
+
+    // Input rows and columns
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    // Check multiplication condition
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible.\n");
+        return 0;
+    }
+
+    // Input first matrix
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c1; j++)
+            scanf("%d", &a[i][j]);
+
+    // Input second matrix
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < r2; i++)
+        for (int j = 0; j < c2; j++)
+            scanf("%d", &b[i][j]);
+
+    // Matrix multiplication logic
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            mul[i][j] = 0;
+            for (int k = 0; k < c1; k++) {
+                mul[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    // Print result
+    printf("\nResult of matrix multiplication:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", mul[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
