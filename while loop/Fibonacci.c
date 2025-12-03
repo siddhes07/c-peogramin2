67ebbffa603193350b0a8efb2eec341ce809d715
siddhes07 <siddheshnj07@gmail.com>
2025-12-03 23:26:52 +0530

    Fibonacci

---
+++ while loop/Fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i = 1;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(i <= n) {
+        fact = fact * i;
+        i++;
+    }
+
+    printf("Factorial of %d = %lld\n", n, fact);
+
+    return 0;
+}
