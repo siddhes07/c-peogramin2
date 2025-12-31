32c88be63c8ceb7278bf0baed05b1e967930d7e2
siddhes07 <siddheshnj07@gmail.com>
2025-12-31 23:20:03 +0530

    Add loop to skip odd numbers and print evens
    Implement a for loop to skip odd numbers and print even numbers from 1 to 20.

---
+++ for loop/Skip_odd_num.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for(i = 1; i <= 20; i++) {
+        if(i % 2 != 0) {
+            continue;   
+        }
+        printf("%d ", i);   
+    }ý
+
+    return 0;
+}
