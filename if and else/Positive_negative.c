8453b6420a40a84273aa8ec5a11c9b6591ccc763
siddhes07 <siddheshnj07@gmail.com>
2026-01-02 00:16:54 +0530

    Add Positive_negative.c to check number sign

---
+++ if and else/Positive_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num >= 0) {
+        printf("The number is Positive");
+    } else {
+        printf("The number is Negative");
+    }
+
+    return 0;
+}
