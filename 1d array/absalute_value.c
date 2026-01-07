fcd1191bb96793ebcaf32f925e0aeba44522ea9e
siddhes07 <siddheshnj07@gmail.com>
2026-01-07 13:33:14 +0530

    Add files via upload

---
+++ 1d array/absalute_value.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the number:\n");
+    scanf("%d",&n);
+    if(n<0){
+        n=-n;{
+            printf("absolute value is %d\n",n);
+        }
+    }
+    return 0;
+}
