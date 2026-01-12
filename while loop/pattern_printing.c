c06db16c4d57694eb560f6c901cfe8d644e722cf
siddhes07 <siddheshnj07@gmail.com>
2026-01-12 13:33:36 +0530

    Add files via upload

---
+++ while loop/pattern_printing.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+    while(i<=5){
+        int j=1;
+        while(j<=i){
+            printf("* ");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
