5eadbc55b5bca62cbac7d1520b00fb840ac0a309
siddhes07 <siddheshnj07@gmail.com>
2026-02-07 21:02:21 +0530

    Add twoSum function in Twosome.cpp
    Implement twoSum function to find indices of two numbers that add up to a target.

---
+++ Twosome.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            if (mp.find(need) != mp.end()) {
+                return {mp[need], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = obj.twoSum(nums, target);
+
+    cout << "Indices: ";
+    for (int i : result) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
