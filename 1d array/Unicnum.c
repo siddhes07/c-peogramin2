2be806adfa774465deae9f3162b8fedb5b8a4b29
siddhes07 <siddheshnj07@gmail.com>
2026-03-08 02:34:31 +0530

    Add program to print unique elements from array

---
+++ 1d array/Unicnum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, count;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Unique elements are:\n");
+
+    for(i = 0; i < n; i++) {
+        count = 0;
+
+        for(j = 0; j < n; j++) {
+            if(arr[i] == arr[j] && i != j) {
+                count++;
+                break;
+            }
+        }
+
+        if(count == 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
