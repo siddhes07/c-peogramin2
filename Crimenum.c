f4769594a10a9e6b1c8c99d9c2f1e78a720c5e89
siddhes07 <siddheshnj07@gmail.com>
2026-08-09 22:54:09 +0530

    Add prime number checker in Crimenum.c

---
+++ Crimenum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 1;
+
+    scanf("%d", &n);
+
+    if (n < 2)
+        flag = 0;
+
+    for (i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
